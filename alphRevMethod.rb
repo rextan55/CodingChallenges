8ffc4b89c26bc9a4e67a84e8da4c74e8c7849503
RexTan55 <RexTan55@gmail.com>
2017-03-08 13:45:37 -0800

    Update alpharev

--- alphRevMethod.rb
+++ alphRevMethod.rb
@@ -1,18 +1,25 @@
-#def AlphaReverse(str)
-a = ('a'..'z').to_a
-
-str = "This is a test"
-str = str.downcase
-
-str = str.split(//).to_a
-
-i = 0
-while i < str.length
+def AlphaReverse(str)
+  a = ('a'..'z').to_a
+  str = str.downcase
+  str = str.split(//).to_a
+  str = str.compact
+  i = 0
+  result = []
+  while i < str.length
+    if str[i] == ' '
+      result << str[i]
+      i = i+1
+  end
   x = str[i]
   y = a.reverse.index(x)
-  puts a[y]
+  result << a[y]
   i += 1
+  end
+  puts result.join
 end
+
+
+AlphaReverse(gets.chomp.to_s)
 
   
 
